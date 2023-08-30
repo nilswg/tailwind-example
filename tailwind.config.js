6dd87b90eb38c2ece660789b6299cd784bf3fb6d
nilsonweng <nilsonweng@moneydj.com>
2023-08-30 16:10:25 +0800

    [update] tailwind demo (1) add serve、concurrently (2) 單一 html 說明功能

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,41 +3,41 @@ const plugin = require('tailwindcss/plugin');
 /** @type {import('tailwindcss').Config} */
 module.exports = {
     important: true,
-    content: ['./src/*.tsx'],
+    content: ['./index.html'],
     theme: {
         extend: {},
-        tabSize: [1, 2, 3, 4, 5].reduce((map, i) => {
-            map[i] = i + '';
-            return map;
-        }, {}),
+        // tabSize: [1, 2, 3, 4, 5].reduce((map, i) => {
+        //     map[i] = i + '';
+        //     return map;
+        // }, {}),
     },
     plugins: [
-        plugin(function ({
-            addUtilities,
-            addComponents,
-            matchUtilities,
-            theme,
-        }) {
-            addUtilities({
-                '.nil-red': {
-                    'background-color': '#f44336',
-                },
-            });
-            addComponents({
-                '.nil-card': {
-                    'background-color': '#fff',
-                    'border-radius': '.25rem',
-                    'box-shadow': '0 2px 4px rgba(0,0,0,0.2)',
-                },
-            });
-            matchUtilities(
-                {
-                    tab: (value) => ({
-                        tabSize: value,
-                    }),
-                },
-                { values: theme('tabSize') }
-            );
-        }),
+        // plugin(function ({
+        //     addUtilities,
+        //     addComponents,
+        //     matchUtilities,
+        //     theme,
+        // }) {
+        //     addUtilities({
+        //         '.nil-red': {
+        //             'background-color': '#f44336',
+        //         },
+        //     });
+        //     addComponents({
+        //         '.nil-card': {
+        //             'background-color': '#fff',
+        //             'border-radius': '.25rem',
+        //             'box-shadow': '0 2px 4px rgba(0,0,0,0.2)',
+        //         },
+        //     });
+        //     matchUtilities(
+        //         {
+        //             tab: (value) => ({
+        //                 tabSize: value,
+        //             }),
+        //         },
+        //         { values: theme('tabSize') }
+        //     );
+        // }),
     ],
 };
